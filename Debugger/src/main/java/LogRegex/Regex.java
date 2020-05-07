9b58bba9006c787ce072d894bfb27594ba280e7a
AznekEnimsay <yasmine.mazouz@telecom-sudparis.eu>
2020-05-07 20:38:12 +0200

    Traitement logs déplacement

--- Debugger/src/main/java/LogRegex/Regex.java
+++ Debugger/src/main/java/LogRegex/Regex.java
@@ -16,55 +16,39 @@ public class Regex {
 
     static public void regex (String logFile) throws IOException {
 
-        String Locomotion = "LOCOMOTION";
-        String Orders = "ORDERS";
-        String Lidar = "LIDAR";
-        String LidarProcess = "LIDAR_PROCESS";
-        String Position = "POSITION";
-        String LLDebug = "LL_DEBUG";
-        String Dynamixel = "DYNAMIXEL";
-        String Communication = "COMMUNICATION";
-
         FileInputStream fstream = new FileInputStream(logFile);
         BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
         String log;
         /* read log line by line */
         while ((log = br.readLine()) != null) {
+
+            Matcher logLocomotion = Pattern.compile("LOCOMOTION").matcher(log);
+            Matcher logOrders = Pattern.compile("ORDERS").matcher(log);
+            Matcher logLidar = Pattern.compile("LIDAR").matcher(log);
+            Matcher logLidarProcess = Pattern.compile("LIDAR_PROCESS").matcher(log);
+            Matcher logPosition = Pattern.compile("POSITION").matcher(log);
+            Matcher logLLDebug = Pattern.compile("LL_DEBUG").matcher(log);
+            Matcher logDynamixel = Pattern.compile("DYNAMIXEL").matcher(log);
+            Matcher logCommunication = Pattern.compile("COMMUNICATION").matcher(log);
+
             try {
 
-                Pattern logLocomotion = Pattern.compile(Locomotion);
-                Pattern logOrders = Pattern.compile(Orders);
-                Pattern logLidar = Pattern.compile(Lidar);
-                Pattern logLidarProcess = Pattern.compile(LidarProcess);
-                Pattern logPosition = Pattern.compile(Position);
-                Pattern logLLDebug = Pattern.compile(LLDebug);
-                Pattern logDynamixel = Pattern.compile(Dynamixel);
-                Pattern logCommunication = Pattern.compile(Communication);
 
-                Matcher mlogLocomotion = logLocomotion.matcher(log);
-                Matcher mlogOrders = logOrders.matcher(log);
-                Matcher mlogLidar = logLidar.matcher(log);
-                Matcher mlogLidarProcess = logLidarProcess.matcher(log);
-                Matcher mlogPosition = logPosition.matcher(log);
-                Matcher mlogLLDebug = logLLDebug.matcher(log);
-                Matcher mlogDynamixel = logDynamixel.matcher(log);
-                Matcher mlogCommunication = logCommunication.matcher(log);
-
-                if (mlogLocomotion.find()) {
+                if (logLocomotion.find()) {
                     RegexDeplacement.regexDeplacement(log);
-                } else if (mlogOrders.find()) {
+                } else if (logOrders.find()) {
                     RegexActionsMeca.regexActions(log);
-                } else if (mlogLidar.find()) {
+                } else if (logLidar.find()) {
                     RegexLidar.regexLidar(log);
-                } else if (mlogLidarProcess.find()) {
+                } else if (logLidarProcess.find()) {
                     RegexLidar.regexLidar(log);
-                } else if (mlogPosition.find()) {
+                } else if (logPosition.find()) {
                     //A voir
-                } else if (mlogLLDebug.find()) {
+                } else if (logLLDebug.find()) {
                     //A voir
-                } else if (mlogDynamixel.find()) {
+                } else if (logDynamixel.find()) {
                     RegexActionsMeca.regexActions(log);
-                } else if (mlogCommunication.find()) {
+                } else if (logCommunication.find()) {
                     //Comm buddy ou NUC ?
                 }
 
@@ -76,7 +60,7 @@ public class Regex {
     }
 
     public static void main(String[] args) throws IOException {
-        String logfile = "/home/yasmine/TechTheTide-HighLevel/logs/Sat Mar 07 21:07:46 CET 2020 #0.log";
+        String logfile = "/home/yasmine/TechTheTide-HighLevel/logs/Fri Jan 24 11_04_25 CET 2020 #60.log";
         Regex.regex(logfile);
     }
 }
