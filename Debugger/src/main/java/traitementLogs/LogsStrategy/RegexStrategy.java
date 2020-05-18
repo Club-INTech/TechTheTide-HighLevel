1ccd4c0063fdb3baa9c6d4cb3fdf7eb77cc137d2
LamiaLaBg <lamia.salhi@telecom-sudparis.eu>
2020-05-18 15:11:49 +0200

    ajout des modifications lamia

--- Debugger/src/main/java/traitementLogs/LogsStrategy/RegexStrategy.java
+++ Debugger/src/main/java/traitementLogs/LogsStrategy/RegexStrategy.java
@@ -3,13 +3,15 @@ package traitementLogs.LogsStrategy;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/*obtention de la couleur de la zone de départ des robots*/
 public class RegexStrategy {
-    static public void regexStrategy (String log) {
-        Matcher Configuration = Pattern.compile("couleur").matcher(log);
-
-        if (Configuration.find()) {
-            //String couleur = log.substring(156);
-            //System.out.println("couleur = " + couleur);
+    static public String regexStrategy (String log) {
+        Matcher CouleurDeDepart = Pattern.compile("Couleur").matcher(log);
+        if (CouleurDeDepart.find()) {
+            String couleur = log.substring(114);
+            //System.out.println("couleur = " + getCouleurZone(log));
+            return couleur;
         }
+        return null;
     }
 }
