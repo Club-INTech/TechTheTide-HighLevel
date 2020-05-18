16f91fc236ee486f77daf30f60b5d094a713eb49
LamiaLaBg <lamia.salhi@telecom-sudparis.eu>
2020-05-18 15:12:17 +0200

    modif lamia

--- Debugger/src/main/java/traitementLogs/LogsCommunications/ComNuc/RegexComNuc.java
+++ Debugger/src/main/java/traitementLogs/LogsCommunications/ComNuc/RegexComNuc.java
@@ -1,17 +1,17 @@
-package traitementLogs.LogsCommunications.ComNUC;
+package traitementLogs.LogsCommunications.ComNuc;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexComNuc {
 
-    static public void regexComNuc (String log) {
+    static public void regexComNuc (String log, String couleurZone) {
         Matcher Configuration = Pattern.compile("Configuration de l'ecueil commun :").matcher(log);
 
         if (Configuration.find()) {
             String compo = log.substring(156);
             System.out.println("composition = " + compo);
-            //graphique.Robot.SetEceuilCommun(compo);
+            //graphique.Robot.SetEceuilCommun(compo,couleurZone);
         }
     }
 }
